b60a90568fabaab4132c46ad75a82cde9ce87b6c
김재환 <woghks7859@gmail.com>
2022-09-23 16:39:09 +0900

    FEAT: localStorage 공부

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const logInForm = document.querySelector("#login-form");
 const logInInput = logInForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
 const link = document.querySelector("a");
+const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
 
 function onLogInSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    
+    logInForm.classList.add(HIDDEN_CLASS);
+    const username = logInInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
 }
 
-function onLinkClicked(event) {
-    event.preventDefault();
-    console.dir(event);
+function paintGreeting(username) {
+    greeting.innerText = `Wow ${username}`;
+    greeting.classList.remove(HIDDEN_CLASS);
 }
 
-logInForm.addEventListener("submit", onLogInSubmit);
-link.addEventListener("click", onLinkClicked);
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+    logInForm.classList.remove(HIDDEN_CLASS);
+    logInForm.addEventListener("submit", onLogInSubmit);
+} else {
+    paintGreeting(savedUserName);
+}
