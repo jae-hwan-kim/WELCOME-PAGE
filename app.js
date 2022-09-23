9f2468b93eeca62ca4ca12224c5f72a3fb797658
김재환 <woghks7859@gmail.com>
2022-09-23 10:02:19 +0900

    FEAT: 이벤트 공부

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("hi");
+const h1 = document.querySelector(".hello:first-child h1");
+
+console.dir(h1);
+
+function handleTitleClick() {
+    const clickedClass = "clicked";
+    if (h1.className === clickedClass)
+        h1.className = "";
+    else
+        h1.className = clickedClass;
+}
+
+h1.addEventListener("click", handleTitleClick);
